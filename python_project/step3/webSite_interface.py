387ce088784b1c36f52e75e0e9ff78ba5091c7d0
stormy <abdeladimebenali2003@gmail.com>
2024-03-24 19:34:07 +0000

    hsh

--- python_project/step3/webSite_interface.py
+++ python_project/step3/webSite_interface.py
@@ -1,113 +1,91 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from dataPreprocessingClass import dataPreprocessing
+from python_project.step3.dataPreprocessingClass import dataPreprocessing
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.decomposition import PCA
+st.title("Football Transfer Price Predictor 💰")
+st.video(r"whatsapp-video-2024-03-22-at-171246-ab2a28a1_4aDFAO4r.mp4", start_time=0)  # Add loop=True for infinite looping
+st.write(" "*30 + "**Welcome! Please enter your name and click the button below to proceed:**" + " "*30)
+user_name = st.text_input("Enter Your Name", "")
 
+if st.button("Continue"):
+    st.sidebar.header("User Input Parameters")
+    def user_input_features():
+        age = st.sidebar.slider('Age', 16, 40, 20)
+        positions = ['Attacking Midfield', 'Central Midfield', 'Centre-Back', 'Centre-Forward',
+                 'Defensive Midfield', 'Goalkeeper', 'Left Midfield', 'Left Winger', 'Left-Back',
+                 'Right Midfield', 'Right Winger', 'Right-Back', 'Second Striker']
+        position = st.sidebar.selectbox("Select Player Position", positions) 
+        ATT = st.sidebar.slider('Attacking', 1, 100, 50)
+        SKI = st.sidebar.slider("Skill", 1, 100, 50)
+        MOV = st.sidebar.slider("Movement", 1, 100, 50)
+        POW = st.sidebar.slider("Power", 1, 100, 50)
+        MEN = st.sidebar.slider("Mentality", 1, 100, 50)
+        DEF = st.sidebar.slider("Defending", 1, 100, 50)
+        GK = st.sidebar.slider("Goalkeeping", 1, 100, 50)
 
+        data = {
+            'name': user_name,
+            'age': age,
+            "position": position,
+            'ATT': ATT,
+            'SKI': SKI,
+            'MOV': MOV,
+            'POW': POW,
+            'MEN': MEN,
+            'DEF': DEF,
+            'GK': GK,
+        }
 
+        index = range(1)
+        features = pd.DataFrame(data, index=index)
+        return features
 
+    user_input = user_input_features()
 
-# Building the basics of the website
-st.write("""
-# Player Price Prediction
+    st.subheader('📊 Player Statistics Input')
+    st.write(user_input)
+    instance = dataPreprocessing(user_input)
+    instance.clean_data()
+    instance.encode_categorical_data()
 
-Estimate the **PRICE** of a Player with Machine Learning!
-""")
+    new_order = ['name', 'age', 'ATT', 'SKI', 'MOV', 'POW', 'MEN', 'DEF', 'GK',
+            'position_Attacking Midfield', 'position_Central Midfield',
+           'position_Centre-Back', 'position_Centre-Forward',
+           'position_Defensive Midfield', 'position_Goalkeeper',
+           'position_Left Midfield', 'position_Left Winger', 'position_Left-Back',
+           'position_Right Midfield', 'position_Right Winger',
+           'position_Right-Back', 'position_Second Striker']
+    position = user_input.columns[-1]
+    instance.add_reorder_columns(position, new_order)
 
-st.sidebar.header("user input parameteres")
+    user_input = instance.data_frame().copy()
 
-def user_input_features():
-    name = st.text_input("Enter Player Name", "")
-    age = st.sidebar.slider('Age', 16, 40, 20)
-    positions = ['Attacking Midfield', 'Central Midfield', 'Centre-Back', 'Centre-Forward',
-             'Defensive Midfield', 'Goalkeeper', 'Left Midfield', 'Left Winger', 'Left-Back',
-             'Right Midfield', 'Right Winger', 'Right-Back', 'Second Striker']
-    position = st.selectbox("Select Player Position", positions) 
-    ATT = st.sidebar.slider('Attacking', 1, 100, 50)
-    SKI = st.sidebar.slider("Skill", 1, 100, 50)
-    MOV = st.sidebar.slider("Movement", 1, 100, 50)
-    POW = st.sidebar.slider("Power", 1, 100, 50)
-    MEN = st.sidebar.slider("Mentality", 1, 100, 50)
-    DEF = st.sidebar.slider("Defending", 1, 100, 50)
-    GK = st.sidebar.slider("Goalkeeping", 1, 100, 50)
+    df = pd.read_csv(r"test.csv")
 
-    data = {
-        'name': name,
-        'age': age,
-        'position': position,
-        'ATT': ATT,
-        'SKI': SKI,
-        'MOV': MOV,
-        'POW': POW,
-        'MEN': MEN,
-        'DEF': DEF,
-        'GK': GK,
-    }
+    # Model training section
+    X = df.drop(["Unnamed: 0", "fee", "name", "market_value"], axis=1)
+    pca = PCA(n_components=10)  
 
-    index = range(1)
-    features = pd.DataFrame(data, index=index)
-    return features
+    X_pca = pca.fit_transform(X)
+    y = df["fee"]
 
-user_input = user_input_features()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1937)
 
-st.subheader('User input parameters')
-st.write(user_input)
+    alphas = [0.001, 0.01, 0.1, 1, 10, 100]
+    reg = Ridge()
+    param_grid = {'alpha': alphas}
 
+    grid_search = GridSearchCV(reg, param_grid, cv=5, scoring='neg_mean_squared_error')
 
+    grid_search.fit(X_train, y_train)
 
-# Preprocessing the user data
-instance = dataPreprocessing(user_input)
-instance.clean_data()
-instance.encode_categorical_data()
+    best_reg = grid_search.best_estimator_
 
-new_order = ['name', 'age', 'ATT', 'SKI', 'MOV', 'POW', 'MEN', 'DEF', 'GK',
-        'position_Attacking Midfield', 'position_Central Midfield',
-       'position_Centre-Back', 'position_Centre-Forward',
-       'position_Defensive Midfield', 'position_Goalkeeper',
-       'position_Left Midfield', 'position_Left Winger', 'position_Left-Back',
-       'position_Right Midfield', 'position_Right Winger',
-       'position_Right-Back', 'position_Second Striker']
-position = user_input.columns[-1]
-instance.add_reorder_columns(position, new_order)
+    # Predicting the estimated price
+    y_pred = best_reg.predict(user_input.drop("name", axis=1))
 
-user_input = instance.data_frame().copy()
-
-
-
-
-# Reading the training data for the model
-df = pd.read_csv(r"C:\Users\Badr Lakhal\Downloads\model_training_data.csv")
-
-
-
-# Model trainig section
-X = df.drop(["Unnamed: 0", "name", "fee"], axis=1)
-pca = PCA(n_components=10)  
-
-X_pca = pca.fit_transform(X)
-y = df["fee"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1937)
-
-alphas = [0.001, 0.01, 0.1, 1, 10, 100]
-reg = Ridge()
-param_grid = {'alpha': alphas}
-
-grid_search = GridSearchCV(reg, param_grid, cv=5, scoring='neg_mean_squared_error')
-
-grid_search.fit(X_train, y_train)
-
-best_reg = grid_search.best_estimator_
-
-# Estimating the player's price
-y_pred = best_reg.predict(user_input.drop("name", axis=1))
-
-
-
-
-# Printing the estimated price on the website
-st.subheader("Prediction in millions of euros")
-st.write(abs(np.round(y_pred)))
+    st.subheader("Prediction in millions of euros 💶 ")
+    st.write(abs(np.round(y_pred)))
